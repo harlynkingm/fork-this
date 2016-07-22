63f2f8163227d6271e277062b6d96aa1ecf92b27
Max Harlynking <mharlynk@yelp.com>
2016-07-22 14:13:59 -0700

    Fixed syntax

--- client/js/app.js
+++ client/js/app.js
@@ -1,11 +1,11 @@
 import $ from 'jquery'
 
-const API_BASE_PATH = 'http://interndev1-uswest1adevc'
-const API_URL = 'api/v1/'
+const API_BASE_PATH = 'http://interndev1-uswest1adevc';
+const API_URL = 'api/v1/';
 
-let ANIMATING = false
+let ANIMATING = false;
 let LOAD_COUNT = 0;
-let SESSION_DONE = false
+let SESSION_DONE = false;
 
 let IMAGES = [
     'https://s3-media3.fl.yelpcdn.com/bphoto/kODDC-4FivgtRxMh2uFvDw/o.jpg',
@@ -19,7 +19,7 @@ let IMAGES = [
     'https://s3-media1.fl.yelpcdn.com/bphoto/5CzBk137CIubKSF5uu4rzQ/o.jpg',
     'https://s3-media3.fl.yelpcdn.com/bphoto/5sRw-gG5p9C0j8zhfsHwIA/o.jpg',
     'https://s3-media3.fl.yelpcdn.com/bphoto/0sv_o29riplN34HQSJEyLQ/o.jpg'
-]
+];
 
 let IMAGE_LABELS = [
     ['thai', 'pasta', 'asian'],
@@ -32,27 +32,27 @@ let IMAGE_LABELS = [
     ['pasta', 'italian'],
     ['salad', 'american'],
     ['salad', 'asian'],
-    ['pizza', 'italian'],
-]
+    ['pizza', 'italian']
+];
 
 let IMG_INDEX_1 = 0;
 let IMG_INDEX_2 = 0;
 
-let USER_TAGS = {}
+let USER_TAGS = {};
 
 
 const getUserPosition = () => {
     return new Promise((resolve, reject) => {
-        navigator.geolocation.getCurrentPosition(resolve, reject)
-    })
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
 }
 
 const startSession = (geolocation) => {
-    const loc = geolocation.coords
+    const loc = geolocation.coords;
     return $.ajax({
         method: 'GET',
         url: API_URL + 'start_session/' + loc.latitude + '/' + loc.longitude
-    })
+    });
 }
 
 
@@ -69,14 +69,14 @@ const renderImages = (data) => {
             let rand2 = getRandomInt(0, IMAGES.length - 1);
             while (rand1 == rand2){
                 rand2 = getRandomInt(0, IMAGES.length - 1);
-            }
+            };
             [IMG_INDEX_1, IMG_INDEX_2] = [rand1, rand2];
             $('#img1').css('transform', 'translateX(-300px)');
             $('#img2').css('transform', 'translateX(300px)');
-            $('#img1').css('backgroundImage', `url(${IMAGES[rand1]})`)
-            $('#img2').css('backgroundImage', `url(${IMAGES[rand2]})`)
-            resizeImages()
-            resolve()
+            $('#img1').css('backgroundImage', `url(${IMAGES[rand1]})`);
+            $('#img2').css('backgroundImage', `url(${IMAGES[rand2]})`);
+            resizeImages();
+            resolve();
         } else {
             let most = 0;
             let cat = '';
@@ -87,35 +87,35 @@ const renderImages = (data) => {
                 }
             }
             const data = data || { result: { search_result: { category: cat } } };
-            reject(data)
+            reject(data);
         }
-    })
+    });
 }
 
 const bindClickHandlers = ({session_id}) => {
-    $('#img1').on('click', makeChoice.bind(null, session_id, 1))
-    $('#img2').on('click', makeChoice.bind(null, session_id, 2))
+    $('#img1').on('click', makeChoice.bind(null, session_id, 1));
+    $('#img2').on('click', makeChoice.bind(null, session_id, 2));
 }
 
 const makeChoice = (sessionId, choice, event) => {
-    ANIMATING = true
-    LOAD_COUNT++
+    ANIMATING = true;
+    LOAD_COUNT++;
     if (LOAD_COUNT == 2){
         $("#instructions").css('opacity', 0);
         setTimeout(() => {
-            $('#instructions').html("After a few selections, we'll recommend a restaurant for you.")
-            $('#instructions').css({'opacity': 1})
-        }, 400)
+            $('#instructions').html("After a few selections, we'll recommend a restaurant for you.");
+            $('#instructions').css({'opacity': 1});
+        }, 400);
     }
     if (LOAD_COUNT == 8 || IMAGES.length < 2){
-        SESSION_DONE = true
+        SESSION_DONE = true;
     }
     if (choice == 1){
         for (const label of IMAGE_LABELS[IMG_INDEX_1]){
             if (label in USER_TAGS){
                 USER_TAGS[label]++;
             } else {
-                USER_TAGS[label] = 1
+                USER_TAGS[label] = 1;
             }
         }
     } else {
@@ -123,7 +123,7 @@ const makeChoice = (sessionId, choice, event) => {
             if (label in USER_TAGS){
                 USER_TAGS[label]++;
             } else {
-                USER_TAGS[label] = 1
+                USER_TAGS[label] = 1;
             }
         }
     }
@@ -132,36 +132,34 @@ const makeChoice = (sessionId, choice, event) => {
 //    $.get(API_URL + 'get_pictures/' + sessionId + '/' + choice)
 //        .then(renderImages)
 //        .then(slideIn, showEndPage)
-    setTimeout(()=>renderImages({session_done: SESSION_DONE}).then(slideIn, showEndPage), 1000)
+    setTimeout(()=>renderImages({session_done: SESSION_DONE}).then(slideIn, showEndPage), 1000);
 }
 
 const showEndPage = ({result}) => {
-    const category = result.search_result.category
-    console.log(result)
+    const category = result.search_result.category;
     $('#select-picture').toggleClass('hidden');
     $('#ending-page').toggleClass('hidden');
-    $('#recommendation').html(`<a href="https://www.yelp.com/search?find_desc=${category}&find_loc=140+New+Montgomery+St" target="_blank">${category}</a>`)
+    $('#recommendation').html(`<a href="https://www.yelp.com/search?find_desc=${category}&find_loc=140+New+Montgomery+St" target="_blank">${category}</a>`);
 }
 const resizeImages = () => {
-    $('.select-img').height($('.select-img').width())
+    $('.select-img').height($('.select-img').width());
 }
 
 $('#start-button').on('click', function(){
-    const top = $('.logo').position().top + "px"
+    const top = $('.logo').position().top + "px";
     const logoWidth = $(window).width() < 400 ? '40%' : '25%';
     if ($(window).width() < 400) {
-        console.log('less 400')
-        $('.flex-container').css('height', '100%')
+        $('.flex-container').css('height', '100%');
     }
-    $('.flex-container').css('justify-content', 'flex-start')
-    $('.logo').css({'margin-top': top})
+    $('.flex-container').css('justify-content', 'flex-start');
+    $('.logo').css({'margin-top': top});
     $('.logo').animate({'margin-top': '15px', 'max-width': logoWidth}, function(){
         $("#instructions").toggleClass('hidden');
         $('#select-picture').toggleClass('hidden');
         slideIn();
     })
-    $(".subhead").fadeOut(200)
-    $("#start-button").fadeOut(200)
+    $(".subhead").fadeOut(200);
+    $("#start-button").fadeOut(200);
     $('.img-dish').css('opacity', '1');
 })
 
@@ -177,7 +175,7 @@ const twist = (obj, degs) => {
         $(obj).css({
             'transform': `rotateZ(${degs}deg)`,
             'box-shadow': 'inset 0px 0px 0px 20px #fff'
-        })
+        });
     }
 }
 
@@ -204,15 +202,15 @@ $('.select-img').on('mouseout', function(){
             'box-shadow': ''
         });
     }
-})
+});
 
-renderImages({session_done: false})
-bindClickHandlers({session_id: 123})
+renderImages({session_done: false});
+bindClickHandlers({session_id: 123});
 
 getUserPosition()
     .then(startSession)
     .then(renderImages)
-    .then(bindClickHandlers)
+    .then(bindClickHandlers);
 
-$(window).on('resize', resizeImages)
+$(window).on('resize', resizeImages);
 
